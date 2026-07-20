b8616b97db17a9ada43c35566c999c4c7954a016
kayaniumar261-maker <kayaniumar261@gmail.com>
2026-07-20 16:22:17 +0400

    Add STORAGE_KEYS constants for application storage

---
+++ src/data/constants.ts
@@ -0,0 +1,8 @@
+export const STORAGE_KEYS = {
+  clients: "shab_clients",
+  cases: "shab_cases",
+  tasks: "shab_tasks",
+  hearings: "shab_hearings",
+  payments: "shab_payments",
+  staff: "shab_staff",
+};
